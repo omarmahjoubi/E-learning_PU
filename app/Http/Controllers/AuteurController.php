807926a950997f35d8aff585ba3dce7a81721297
Radianovic <hoster07@gmail.com>
2016-05-07 23:43:29 +0200

    Create AuteurController.php

---
+++ app/Http/Controllers/AuteurController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Auteur;
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use Illuminate\Support\Facades\Input;
+
+class ReportsController extends Controller
+{
+
+public function add(Request $request)
+{
+  $auteur = new Auteur();
+  $auteur->name = $request->name;
+  $auteur->save();
+}
+
+public function delete(Auteur $auteur)
+{
+  $auteur->delete();
+  return back();
+}
+
+public function edit(Request $request, Auteur $auteur)
+{
+  $auteur->name = $request->name;
+  $auteur->save();
+  return back();
+}
+
+}
