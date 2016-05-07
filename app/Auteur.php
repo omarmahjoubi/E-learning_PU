60aa722634c48ecb3e73519b0af924d19932d824
Radianovic <hoster07@gmail.com>
2016-05-07 23:59:07 +0200

    Update Auteur.php

--- app/Auteur.php
+++ app/Auteur.php
@@ -1,12 +1,15 @@
 <?php
-
 namespace App;
-
 use Illuminate\Database\Eloquent\Model;
-
 class Auteur extends Model
 {
     public $timestamps = false;
     protected $fillable = ['name'] ;
-    
+    public function creer($name)
+    {
+        $this->name = $name ;
+    }
+    public function lister() {
+        return Auteur::all();
+    }
 }
