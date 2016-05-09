34a087b71773ba1a7e6d4f1d9e034fb6a109d272
Omar Mahjoubi <omarmahjoubim8@gmail.com>
2016-05-09 23:26:14 +0200

    modif de Auteur.php

--- app/Auteur.php
+++ app/Auteur.php
@@ -3,12 +3,14 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 class Auteur extends Model
 {
-    public $timestamps = false;
+   public $timestamps = false;
     protected $fillable = ['name'] ;
+
     public function creer($name)
     {
         $this->name = $name ;
     }
+
     public function lister() {
         return Auteur::all();
     }
